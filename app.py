fa117719368fd4e932b48df4d0f642a34206a9fb
initializer04 <dc957852@gmail.com>
2025-04-26 00:18:00 +0530

    app.py

--- app.py
+++ app.py
@@ -5,8 +5,11 @@ from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
 
-ps = PorterStemmer()
+# Download necessary NLTK datasets
+nltk.download('punkt')
+nltk.download('stopwords')
 
+ps = PorterStemmer()
 
 def transform_text(text):
     text = text.lower()
@@ -32,22 +35,22 @@ def transform_text(text):
 
     return " ".join(y)
 
-tfidf = pickle.load(open('vectorizer.pkl','rb'))
-model = pickle.load(open('model.pkl','rb'))
+# Load vectorizer and model (ensure they are correctly uploaded to Render)
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
 st.title("Email/SMS Spam Classifier")
 
 input_sms = st.text_area("Enter the message")
 
 if st.button('Predict'):
-
     # 1. preprocess
     transformed_sms = transform_text(input_sms)
     # 2. vectorize
     vector_input = tfidf.transform([transformed_sms])
     # 3. predict
     result = model.predict(vector_input)[0]
-    # 4. Display
+    # 4. Display result
     if result == 1:
         st.header("Spam")
     else:
